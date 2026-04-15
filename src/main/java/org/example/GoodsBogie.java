faf585cfbd9aca88d9f87fe9e1cd443e5dbdaece
Jananir3011 <jananir3011@gmail.com>
2026-04-15 13:57:08 +0530

    UC15: Safe Cargo Assignment Using try-catch-finally

--- src/main/java/org/example/GoodsBogie.java
+++ src/main/java/org/example/GoodsBogie.java
@@ -5,9 +5,8 @@ public class GoodsBogie {
     private String type;
     private String cargo;
 
-    public GoodsBogie(String type, String cargo) {
+    public GoodsBogie(String type) {
         this.type = type;
-        this.cargo = cargo;
     }
 
     public String getType() {
@@ -16,5 +15,28 @@ public class GoodsBogie {
 
     public String getCargo() {
         return cargo;
+    }
+
+    // UC15
+    public void assignCargo(String cargo) {
+
+        try {
+            if (type.equalsIgnoreCase("Rectangular") &&
+                    cargo.equalsIgnoreCase("Petroleum")) {
+
+                throw new CargoSafetyException(
+                        "Unsafe cargo assignment: Petroleum not allowed in Rectangular bogie"
+                );
+            }
+
+            this.cargo = cargo;
+            System.out.println("Cargo assigned: " + cargo);
+
+        } catch (CargoSafetyException e) {
+            System.out.println("ERROR: " + e.getMessage());
+
+        } finally {
+            System.out.println("Cargo assignment attempt completed.\n");
+        }
     }
 }
