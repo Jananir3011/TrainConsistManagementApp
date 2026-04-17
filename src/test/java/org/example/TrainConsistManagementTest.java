b516b5714e0241218892a8983e484318675c810b
Jananir3011 <jananir3011@gmail.com>
2026-04-17 19:15:49 +0530

    UC16: Sort Passenger Bogies by Capacity (Bubble Sort – Algorithm Intro)

--- src/test/java/org/example/TrainConsistManagementTest.java
+++ src/test/java/org/example/TrainConsistManagementTest.java
@@ -31,29 +31,50 @@ public class TrainConsistManagementTest {
     @Test
     void testCargo_SafeAssignment() {
         GoodsBogie b = new GoodsBogie("Cylindrical");
-
         b.assignCargo("Petroleum");
-
         assertEquals("Petroleum", b.getCargo());
     }
 
     @Test
     void testCargo_UnsafeAssignmentHandled() {
         GoodsBogie b = new GoodsBogie("Rectangular");
-
         b.assignCargo("Petroleum");
-
         assertNull(b.getCargo());
     }
 
+    // UC16
     @Test
-    void testCargo_ProgramContinuesAfterException() {
-        GoodsBogie b1 = new GoodsBogie("Rectangular");
-        GoodsBogie b2 = new GoodsBogie("Cylindrical");
+    void testSort_BasicSorting() {
+        int[] input = {72, 56, 24, 70, 60};
+        assertArrayEquals(new int[]{24, 56, 60, 70, 72},
+                TrainConsistManagement.bubbleSortCapacities(input));
+    }
 
-        b1.assignCargo("Petroleum");
-        b2.assignCargo("Petroleum");
+    @Test
+    void testSort_AlreadySortedArray() {
+        int[] input = {24, 56, 60, 70, 72};
+        assertArrayEquals(new int[]{24, 56, 60, 70, 72},
+                TrainConsistManagement.bubbleSortCapacities(input));
+    }
 
-        assertEquals("Petroleum", b2.getCargo());
+    @Test
+    void testSort_DuplicateValues() {
+        int[] input = {72, 56, 56, 24};
+        assertArrayEquals(new int[]{24, 56, 56, 72},
+                TrainConsistManagement.bubbleSortCapacities(input));
+    }
+
+    @Test
+    void testSort_SingleElementArray() {
+        int[] input = {50};
+        assertArrayEquals(new int[]{50},
+                TrainConsistManagement.bubbleSortCapacities(input));
+    }
+
+    @Test
+    void testSort_AllEqualValues() {
+        int[] input = {40, 40, 40};
+        assertArrayEquals(new int[]{40, 40, 40},
+                TrainConsistManagement.bubbleSortCapacities(input));
     }
 }
